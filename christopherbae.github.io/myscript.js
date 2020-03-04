fff092be7dcd30df356ee9f831bd0eba332f2951
Christopher Bae <cb201299@gmail.com>
2020-03-04 18:39:31 -0500

    added database stuff

--- christopherbae.github.io/myscript.js
+++ christopherbae.github.io/myscript.js
@@ -1,7 +1,20 @@
 function getstarted(){
     window.location.href = "login.html";
 }
+// Set the configuration for your app
+const firebaseConfig = {
+    apiKey: "AIzaSyDVYno5lUC1wnepnelL58WNy4rpStu21jY",
+    authDomain: "depresso-d7acf.firebaseapp.com",
+    databaseURL: "https://depresso-d7acf.firebaseio.com",
+    projectId: "depresso-d7acf",
+    storageBucket: "depresso-d7acf.appspot.com",
+    messagingSenderId: "347690304511",
+    appId: "1:347690304511:web:585640e7fc137dc33a1dca",
+    measurementId: "G-DCWKDV6QJE"
+};
+function next(){
 
-function login(){
-    window.location.href = "home.html";
+    window.location.href = "nextpage.html";
 }
+
+
